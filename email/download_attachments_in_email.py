a0f992713f9075888f0ac09734e7b181a54f31d7
tom-sapletta-com <tom@sapletta.com>
2023-07-27 15:53:16 +0200

    Update logic

--- email/download_attachments_in_email.py
+++ email/download_attachments_in_email.py
@@ -1,13 +1,13 @@
 import email
 
-def download_attachments_in_email(m, emailid="", outputdir="", xx=1):
+def download_attachments_in_email(m, emailid="", outputdir=""):
     #m.store(emailid, '+FLAGS', '\Seen')
     print("download_attachments_in_email emailid:", emailid)
     print("download_attachments_in_email outputdir:", outputdir)
-    print("download_attachments_in_email xx:", xx)
 
     resp, data = m.fetch(str(emailid), "(BODY.PEEK[])")
-    print("mail emailid:", emailid)
+    print("download_attachments_in_email emailid:", emailid)
+
     #resp, data = m.fetch(emailid, '(RFC822)')
     print("mail respo:", resp)
 
@@ -15,16 +15,18 @@ def download_attachments_in_email(m, emailid="", outputdir="", xx=1):
     mail = email.message_from_bytes(email_body)
     if mail.get_content_maintype() != 'multipart':
         return
+    xx=0
     for part in mail.walk():
+        xx=xx+1
+        filename = str(emailid) + "_" + str(xx)
+
         if part.get_content_maintype() != 'multipart' and part.get('Content-Disposition') is not None:
             try:
                 open(outputdir + '/' + part.get_filename(), 'wb').write(part.get_payload(decode=True))
                 #print(part.get_filename())
             except TypeError as ex:
-                #open(outputdir + '/' + str(xx) + ".txt", 'wb').write(part.get_payload(decode=True))
-                open(outputdir + '/' + str(xx) + ".html", 'wb').write(part.get_payload(decode=True))
+                open(outputdir + '/' + filename + ".html", 'wb').write(part.get_payload(decode=True))
             except FileNotFoundError as ex:
-                filename = str(xx)
                 for character in part.get_filename():
                     if character.isalnum():
                         filename += character
