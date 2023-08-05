7d0e9e1802941c29be616d92bef9c3293d706550
tom-sapletta-com <tom@sapletta.com>
2023-08-05 16:23:25 +0200

    update files

--- markdown/create_dir_structure_from_headers.py
+++ markdown/create_dir_structure_from_headers.py
@@ -1,14 +1,30 @@
 import re
+import os
+import sys
 
-def create_dir_structure_from_headers(filename, base_dir):
+sys.path.append('../')
+from pyfunc.markdown.get_url_list import get_url_list
+from pyfunc.markdown.get_header_list import get_header_list
 
-    with open(filename, 'r') as f:
-        content = f.read()
 
-    # Regular expression pattern for Markdown headers
-    pattern = r'^#{1,6}\s+(.*)$'
+# markdown_file
+# source - path to source folder
+# pattern - regular expression pattern for Markdown headers
+def create_dir_structure_from_headers(markdown_file="",
+                                      path="",
+                                      pattern_list=[r'^#{1,6}\s+(.*)$', r'\[([^\]]+)\]\(([^)]+)\)']):
+    with open(markdown_file, 'r') as file:
+        markdown = file.read()
 
-    headers = re.findall(pattern, content, re.MULTILINE)
+    for header in get_header_list(markdown, pattern_list[0]):
+        urls = [header]
+        print(urls)
+        #exit()
+        for url in get_url_list(header, pattern_list[1]):
+            path_folder = os.path.join(path, str(url))
+            print(path_folder)
+            #exit()
+            if not os.path.exists(path_folder):
+                os.makedirs(path_folder)
 
-    for header in headers:
-        print(header)
+
