2f3665cb9d780de0e5d66c3ab1fa06d05b88805d
tom-sapletta-com <tom@sapletta.com>
2023-08-19 16:45:47 +0200

    update

--- markdown/create_dir_structure.py
+++ markdown/create_dir_structure.py
@@ -2,13 +2,14 @@ import os
 import mistune
 from mistune import HTMLRenderer
 
+
 def create_dir_structure(md_text, base_dir):
-    #renderer = HeaderRenderer()
-    #renderer = mistune.HTMLRenderer()
+    # renderer = HeaderRenderer()
+    # renderer = mistune.HTMLRenderer()
     renderer = mistune.HTMLRenderer()
-    #markdown = mistune.Markdown(renderer, plugins=[strikethrough])
+    # markdown = mistune.Markdown(renderer, plugins=[strikethrough])
     markdown = mistune.Markdown(renderer)
-    #markdown = mistune.Markdown(renderer=renderer)
+    # markdown = mistune.Markdown(renderer=renderer)
     headers = markdown(md_text).split('\n')
     print(headers)
 
@@ -18,3 +19,10 @@ def create_dir_structure(md_text, base_dir):
             print(pathf)
             os.makedirs(pathf, exist_ok=True)
 
+
+def test(base_dir, filename):
+    # Open and read markdown file
+    with open(filename, "r") as md_file:
+        md_text = md_file.read()
+
+    create_dir_structure(md_text, base_dir)
