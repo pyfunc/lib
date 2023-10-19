302d1bbc75d0dc078afb8f07f93f4b71a8b15a7d
tom-sapletta-com <tom@sapletta.com>
2023-10-19 16:47:56 +0200

    update files

--- file/check_and_create_path.py
+++ file/check_and_create_path.py
@@ -8,7 +8,7 @@ def check_and_create_path(dest_path):
     path = Path(dest_path)
     if not os.path.exists(str(path)):
         try:
-            path.mkdir(parents=False, exist_ok=False)
+            path.mkdir(parents=True, exist_ok=False)
         except FileExistsError:
             print(f'check_and_create_path exist: {dest_path}')
         else:
