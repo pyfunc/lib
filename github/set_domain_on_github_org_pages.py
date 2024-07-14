73aaeb642de0827c96c914f107eddb28770fe0ce
Tom Sapletta <tom@sapletta.com>
2024-07-14 11:44:33 +0200

    fixed headers

--- github/set_domain_on_github_org_pages.py
+++ github/set_domain_on_github_org_pages.py
@@ -7,10 +7,12 @@ from github.getHeaders import getHeaders
 
 # List all repositories in the organization
 # Set your custom domain (must be configured in your DNS) for each repository in Organisation
-def set_domain_on_github_org_pages(api_token, org_name, domain):
+def set_domain_on_github_org_pages(api_token, org_name, domain, headers):
+    if not headers:
+        headers = getHeaders(api_token)
     # List all repositories in the organization
     repos_url = f'https://api.github.com/orgs/{org_name}/repos'
-    response = requests.get(repos_url, headers=getHeaders(api_token))
+    response = requests.get(repos_url, headers=headers)
 
     if response.status_code != 200:
         print('Failed to retrieve repositories:', response.content)
