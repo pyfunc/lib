17206ad782eb32578c5303192a4375e550e3a1b3
Tom Softreck <info@softreck.dev>
2025-04-15 20:24:52 +0200

    Release version 0.1.24

--- src/pyfunc2/_version.py
+++ src/pyfunc2/_version.py
@@ -1,6 +1,6 @@
 """Version information for pyfunc2 package."""
 
-__version__ = version = "0.1.23"
+__version__ = version = "0.1.24"
 
 def get_version():
     """Return the current package version."""
