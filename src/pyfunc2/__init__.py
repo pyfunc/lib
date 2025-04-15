77902cbeac5d8103162bbf6fb897bb9ed3374770
Tom Softreck <info@softreck.dev>
2025-04-15 19:54:32 +0200

    Release version 0.1.10

--- src/pyfunc2/__init__.py
+++ src/pyfunc2/__init__.py
@@ -5,9 +5,9 @@
 from ._version import __version__
 
 # Import necessary modules and functions here
-from img_to_base64 import img_to_base64
-from to_lower_case import convert_char
-from to_lower_case import to_lower_case
+from .img_to_base64 import img_to_base64
+from .to_lower_case import convert_char
+from .to_lower_case import to_lower_case
 
 # Public API of the package
-__all__ = ['__version__', img_to_base64, convert_char, to_lower_case]
+__all__ = ['__version__', 'img_to_base64', 'convert_char', 'to_lower_case']
--- src/pyfunc2/_version.py
+++ src/pyfunc2/_version.py
@@ -1,6 +1,6 @@
 """Version information for pyfunc2 package."""
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 def get_version():
     """Return the current package version."""
