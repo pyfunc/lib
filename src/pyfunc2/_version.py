e41ce5ef60aacbdb28cdbfdcf4eb720f77d801a6
Tom Softreck <info@softreck.dev>
2025-04-15 19:46:08 +0200

    Release version 0.1.7

--- src/pyfunc2/_version.py
+++ src/pyfunc2/_version.py
@@ -1,11 +1,1 @@
-"""
-Provides pyfunc2 version information.
-"""
-
-# This file is auto-generated! Do not edit!
-# Use `python -m incremental.update pyfunc2` to change this file.
-
-from incremental import Version
-
-__version__ = Version("pyfunc2", 24, 7, 0)
-__all__ = ["__version__"]
+__version__ = "0.1.5"
