1d72e6b78f7a87cb6e595225b7ddd19f6c881bf3
Tom Softreck <info@softreck.dev>
2025-04-16 15:52:53 +0200

    Release version 0.1.48

--- src/pyfunc2/ocr/__init__.py
+++ src/pyfunc2/ocr/__init__.py
@@ -19,7 +19,6 @@ from .get_date_from_pdf import remove_all_spaces
 from .get_date_from_pdf import remove_only_single_spaces
 from .get_date_from_pdf import get_date_from_pdf
 from .get_date_from_pdf_pattern import get_date_from_pdf_pattern
-from .get_company_from_pdf import get_company_from_pdf
 
 # Public API of the package
-__all__ = [CompanyList, convert_pdf_to_string, find_string_in_file_path, find_string_in_pdf, from_folder_to_year, from_month_to_company, convertPdf2String, remove_extra_spaces, remove_all_spaces, remove_only_single_spaces, get_company_from_pdf, find_company, convertPdf2String, remove_extra_spaces, remove_all_spaces, remove_only_single_spaces, get_date_from_pdf, get_date_from_pdf_pattern, get_company_from_pdf]
+__all__ = [CompanyList, convert_pdf_to_string, find_string_in_file_path, find_string_in_pdf, from_folder_to_year, from_month_to_company, convertPdf2String, remove_extra_spaces, remove_all_spaces, remove_only_single_spaces, get_company_from_pdf, find_company, convertPdf2String, remove_extra_spaces, remove_all_spaces, remove_only_single_spaces, get_date_from_pdf, get_date_from_pdf_pattern]
