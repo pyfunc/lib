1ed06d024d2c1cb77bdb0787c666b1cfe43a04d4
Tom Softreck <info@softreck.dev>
2025-04-16 16:08:17 +0200

    Release version 0.1.52

--- src/pyfunc2/ml/__init__.py
+++ src/pyfunc2/ml/__init__.py
@@ -1,10 +1,5 @@
 # Auto-generated __init__.py
 
-# Version of the pyfunc2 package
-import sys
-sys.path.append('../')
-from ._version import __version__
-
 # Import necessary modules and functions here
 
 # Public API of the package
